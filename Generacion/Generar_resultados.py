6042b56f0575327fe30e4f7490d701055381ba54
Eligio-Cazo <83460108+Eligio-Cazo@users.noreply.github.com>
2023-09-15 10:40:51 -0400

    Add files via upload

--- Generacion/Generar_resultados.py
+++ Generacion/Generar_resultados.py
@@ -1,20 +1,19 @@
 # Estadisticas probabilisticas eliminatorias sudamericanas
 # Autor Ing. Eligio Cazo Tel 595981500340
-#22/06/2023
+#15/09/2023
 # Revision 11/09/2023 se corrigio funcion puntajes, posiciones
 
-import csv
-import math
+#import csv
+#import math
 import operator
-import os
+#import os
 import random
 import sqlite3
-import tkinter as tk
-from tkinter import ttk
+#import tkinter as tk
+#from tkinter import ttk
 
 import pandas as pd
 from openpyxl import load_workbook
-
 
 def estadistica_real():
     con = sqlite3.connect('datos.db')
@@ -135,168 +134,6 @@ def color():
     color_hex = '#{:02x}{:02x}{:02x}'.format(*color_rgb)
 
     print(color_hex)  # Imprimir el código hexadecimal del color
-
-def puntajes():
-    equipos = {'Brasil':0,'Argentina':1,'Uruguay':2,'Ecuador':3,'Colombia':4,'Chile':5,'Paraguay':6,'Bolivia':7,'Venezuela':8,'Peru':9}
-    suma= {'0':0,'1':0,'2':0,'3':-3,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-    pjugados= {'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-    pganados= {'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-    pempatados= {'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-    pperdidos= {'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-    golesafavor= {'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-    golescontra= {'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-    indices= ['0','1','2','3','4','5','6','7','8','9']
-    pos= ['1','2','3','4','5','6','7','8','9','10']
-
-    a=[]
-    b=[]
-    r=[]
-
-    df = pd.read_excel(io = "fixture.xlsx", sheet_name="Fechas",header = 0)
-    df.fillna(999, inplace=True)
-    fecha = df['Fecha'].values
-    local = df['EquipoLocal'].values
-    marcador1 = df['Marcador1'].values
-    marcador2 = df['Marcador2'].values
-    visitante = df['EquipoVisitante'].values
-
-    for i in range(0,90):
-        l=local[i]
-        indice1=equipos[local[i]]
-        indice2=equipos[visitante[i]]
-        v1=marcador1[i]
-        v2=marcador2[i]
-        if v1>v2:
-            r1=1
-        if v1==v2:
-            r1=2
-        if v1<v2:
-            r1=3
-        if v1==999 or v2==999:
-            r1=0
-        a.append(indice1)
-        b.append(indice2)
-        r.append(r1)
-
-    for i in range(0,90):
-        v1=marcador1[i]
-        v2=marcador2[i]
-        if r[i]==0:
-            y=0
-        else:
-            y=r[i]
-        
-        indicea = str(a[i])
-        indiceb = str(b[i])
-        indice1 = indicea.replace(" ", "")
-        indice2 = indiceb.replace(" ", "")
-        
-        if y == 1:
-            suma[indice1] = suma[indice1] + 3
-            pganados[indice1] = pganados[indice1]+1
-            pperdidos[indice2] =pperdidos[indice2]+1
-            pjugados[indice1] = pjugados[indice1]+1
-            pjugados[indice2] = pjugados[indice2]+1
-            golesafavor[indice1]=golesafavor[indice1]+v1
-            golescontra[indice1]=golescontra[indice1]-v2
-            golesafavor[indice2]=golesafavor[indice2]+v2
-            golescontra[indice2]=golescontra[indice2]-v1
-
-        if y == 2:
-            suma[indice1] = suma[indice1] + 1
-            suma[indice2] = suma[indice2] + 1
-            pempatados[indice1] = pempatados[indice1] + 1
-            pempatados[indice2] = pempatados[indice2] + 1
-            pjugados[indice1] = pjugados[indice1]+1
-            pjugados[indice2] = pjugados[indice2]+1
-            golesafavor[indice1]=golesafavor[indice1]+v1
-            golescontra[indice1]=golescontra[indice1]-v2
-            golesafavor[indice2]=golesafavor[indice2]+v2
-            golescontra[indice2]=golescontra[indice2]-v1
-
-        if y == 3:
-            suma[indice2] = suma[indice2] + 3
-            pganados[indice2] = pganados[indice2]+1
-            pperdidos[indice1] =pperdidos[indice1]+1
-            pjugados[indice1] = pjugados[indice1]+1
-            pjugados[indice2] = pjugados[indice2]+1
-            golesafavor[indice1]=golesafavor[indice1]+v1
-            golescontra[indice1]=golescontra[indice1]-v2
-            golesafavor[indice2]=golesafavor[indice2]+v2
-            golescontra[indice2]=golescontra[indice2]-v1
-        
-    indice=posiciones(suma)
-    lista=ordena_matriz(suma)
-    listapais=[]
-    puntaje=[]
-    pj=[]
-    pg=[]
-    pe=[]
-    pp=[]
-    gf=[]
-    gc=[]
-    dg=[]
-
-    for i in indice:
-        claves = [clave for clave, val in equipos.items() if val == int(i)]
-        contenido=claves[0] + " " + str(suma[i]) +" "+str(pjugados[i])+" "+str(pganados[i])+" "+str(pempatados[i])+" "+str(pperdidos[i])+" "+str(golesafavor[i])+" " + str(abs(golescontra[i]))+ " "+ str(golesafavor[i]+golescontra[i])
-        listapais.append(claves[0])
-        puntaje.append(int(suma[i]))
-        pj.append(int(pjugados[i]))
-        pg.append(int(pganados[i]))
-        pe.append(int(pempatados[i]))
-        pp.append(int(pperdidos[i]))
-        gf.append(int(golesafavor[i]))
-        gc.append(int(golescontra[i]))
-        dg.append(int(golesafavor[i]+golescontra[i]))
-
-    if pg[0]==0:
-        listapais=[]
-        puntaje=[]
-        for i in indices:
-            claves = [clave for clave, val in equipos.items() if val == int(i)]
-            listapais.append(claves[0])
-            #print(claves[0])
-            puntaje.append(suma[i])
-
-    # Crear el DataFrame a partir de las listas
-    df = pd.DataFrame({
-        "PAIS": listapais,
-        "PUNTAJE": puntaje,
-        "PJ": pj,
-        "PG": pg,
-        "PE": pe,
-        "PP": pp,
-        "GF": gf,
-        "GC": gc,
-        "DG": dg
-    })
-    df_ordenado = df.sort_values(['PUNTAJE', 'DG', 'GF',"PAIS"], ascending=[False, False, False,True])
-    df_ordenado = df_ordenado.reset_index(drop=True)
-    df_ordenado.index = df_ordenado.index + 1
-
-
-    archivo_excel = 'fixture.xlsx'
-    book = load_workbook(archivo_excel)
-    writer = pd.ExcelWriter(archivo_excel, engine='openpyxl')
-    writer.book = book
-    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-    df_ordenado.to_excel(writer, sheet_name='Posiciones', index=True)
-    writer.save()
-
-    # Crear la ventana principal de tkinter
-    ventana = tk.Tk()
-    text_widget = tk.Text(ventana)
-    text_widget.pack()
-    df_texto = df_ordenado.to_string()
-    text_widget.insert(tk.END, df_texto)
-    ancho_ventana = 500
-    alto_ventana = 200
-    posicion_x = (ventana.winfo_screenwidth() - ancho_ventana) // 2
-    posicion_y = (ventana.winfo_screenheight() - alto_ventana) // 2
-    ventana.geometry(f"{ancho_ventana}x{alto_ventana}+{posicion_x}+{posicion_y}")
-    ventana.title("Posiciones Eliminatorias Mundial 2026")
-    ventana.mainloop()
 
 def posiciones(stats):
     claves_ordenadas = sorted(stats, key=lambda x: stats[x], reverse=True)
@@ -487,6 +324,8 @@ def fixture3():
     marcador2 = df['Marcador2'].values
     visitante = df['EquipoVisitante'].values
 
+    print(df)
+
     for i in range(0,90):
         l=local[i]
         indice1=equipos[local[i]]
@@ -496,12 +335,16 @@ def fixture3():
     
         if v1>v2:
             r1=1
+        
         if v1==v2:
             r1=2
+        
         if v1<v2:
             r1=3
+        
         if v1==999 or v2==999:
             r1=0
+        
         a.append(indice1)
         b.append(indice2)
         r.append(r1)
